cc340852d9f7b42b2c18130503d9f6055de5c7f1
Marc Clinedinst <marc.clinedinst@fiscalnote.com>
2019-10-17 17:26:07 -0400

    Adding sleeps to tests for demo purposes.

--- tests/cq_login_test.py
+++ tests/cq_login_test.py
@@ -16,15 +16,21 @@ class TestCQLoginPage:
         return driver
 
     @pytest.mark.login
-    def test_login_page(self, login_page_driver):
+    def test_user_sees_error_message_when_attempting_to_log_in_with_invalid_crendentials(self, login_page_driver):
         login_page = LoginPage(login_page_driver)
 
         logging.info('Verifying that the log in error message is not displayed.')
         assert login_page.log_in_error_is_displayed is False
 
+        time.sleep(2)
         login_page.enter_username('this_is_not_a_real_username@email.com')
+
+        time.sleep(2)
         login_page.enter_password('this_is_not_a_real_password')
+
+        time.sleep(2)
         login_page.click_log_in_button()
 
+        time.sleep(2)
         logging.info('Verifying that the log in error message is displayed.')
         assert login_page.log_in_error_is_displayed
